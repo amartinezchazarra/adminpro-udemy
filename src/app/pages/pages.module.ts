165d7c2e0005dff6b2504cf3696a1853bd3a924f
unknown <amartinez.chazarra@yahoo.es>
2019-11-15 14:48:21 +0100

    Modulo PageModule y SharedModule otra vez

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from "@angular/core";
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { SharedModule } from '../shared/shared.module';
+
+//Translation
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        Graficas1Component,
+        ProgressComponent
+    ],
+    exports: [
+        DashboardComponent,
+        Graficas1Component,
+        ProgressComponent
+    ],
+    imports: [
+        SharedModule,
+        // ngx-translate and the loader module
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
+    ]
+})
+
+export class PagesModule{}
+ 
+// required for AOT compilation
+ export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { HeaderComponent } from './header/header.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+//Translation
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+    declarations:[
+        BreadcrumbsComponent,
+        HeaderComponent,
+        NopagefoundComponent,
+        SidebarComponent
+    ],
+    exports: [
+        BreadcrumbsComponent,
+        HeaderComponent,
+        NopagefoundComponent,
+        SidebarComponent
+    ],
+    imports:[
+            // ngx-translate and the loader module
+        HttpClientModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient]
+            }
+        })
+    ]
+})
+
+export class SharedModule{}
+ 
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
+}
